5e36064bf167b3f4196af7b6262a74c7eb555985
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-11-29 21:25:16 +0000

    Add some basic tests

---
+++ test/aoc_day_tests.cpp
@@ -0,0 +1,25 @@
+#include <catch2/catch.hpp>
+
+#include "aoc_day.hpp"
+#include "aoc_days.hpp"
+
+TEST_CASE("GetDay returns nullptr if the day does not exist", "[Main]")
+{
+  AocDays days;
+  auto aoc_day = days.GetDay(0);
+  REQUIRE(aoc_day == nullptr);
+}
+
+TEST_CASE("GetDay returns the day if the day exists", "[Main]")
+{
+  AocDays days;
+  auto aoc_day = days.GetDay(-1);
+  REQUIRE(aoc_day != nullptr);
+}
+
+TEST_CASE("Get the demo day", "[Demo]")
+{
+  AocDays days;
+  auto aoc_day = days.GetDay(-1);
+  REQUIRE(aoc_day != nullptr);
+}
