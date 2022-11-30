fb662023858e10712349aa8ce76d2e2399175e0a
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-11-30 21:14:00 +0000

    Add tests for demo day

--- test/day_solutions/demo/aoc_day_demo_tests.cpp
+++ test/day_solutions/demo/aoc_day_demo_tests.cpp
@@ -9,3 +9,29 @@ TEST_CASE("Get the demo day", "[Demo]")
   auto aoc_day = days.GetDay(-1);
   REQUIRE(aoc_day != nullptr);
 }
+
+TEST_CASE("Test part 1 call", "[Demo]")
+{
+  AocDays days;
+  REQUIRE("Demo solution part 1" == std::get<std::string>(days.GetDay(-1)->SolvePart(1, "/", {})));
+}
+
+TEST_CASE("Test part 2 call", "[Demo]")
+{
+  AocDays days;
+  REQUIRE("Demo solution part 2" == std::get<std::string>(days.GetDay(-1)->SolvePart(2, "/", {})));
+}
+
+TEST_CASE("Test wrong part number", "[Demo]")
+{
+  AocDays days;
+  REQUIRE("Wrong part number!" == std::get<std::string>(days.GetDay(-1)->SolvePart(3, "/", {})));
+}
+
+TEST_CASE("Call parts one by another", "[Demo]")
+{
+  AocDays days;
+  REQUIRE("Demo solution part 1" == std::get<std::string>(days.GetDay(-1)->SolvePart(1, "/", {})));
+  REQUIRE("Demo solution part 2" == std::get<std::string>(days.GetDay(-1)->SolvePart(2, "/", {})));
+  REQUIRE("Wrong part number!" == std::get<std::string>(days.GetDay(-1)->SolvePart(3, "/", {})));
+}
