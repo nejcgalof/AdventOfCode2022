52ed12e65e62b07f8841ca1c5ac9b31b8c0024bc
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-12-01 17:04:20 +0000

    Optimize day one depend on both parts

--- include/day_solutions/day_01/aoc_day_01.hpp
+++ include/day_solutions/day_01/aoc_day_01.hpp
@@ -15,6 +15,8 @@ private:
   std::variant<int, double, std::string> Part2([[maybe_unused]] const std::string &file,
     [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs) override;
 
+  static std::vector<int> DescSortedElfCaloriesFromFile(const std::string &fileName);
+
 public:
   AocDay01();
   ~AocDay01() override;
--- src/day_solutions/day_01/aoc_day_01.cpp
+++ src/day_solutions/day_01/aoc_day_01.cpp
@@ -1,5 +1,6 @@
 #include "day_01/aoc_day_01.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <numeric>
 
@@ -7,46 +8,40 @@ AocDay01::AocDay01() : AocDay(1) {}
 
 AocDay01::~AocDay01() = default;
 
-std::variant<int, double, std::string> AocDay01::Part1([[maybe_unused]] const std::string &file,
-  [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
+std::vector<int> AocDay01::DescSortedElfCaloriesFromFile(const std::string &fileName)
 {
-  int max_calories = 0;
-  std::ifstream file_stream(file);
-  if (file_stream.is_open()) {
+  std::vector<int> elf_calories;
+  std::ifstream file(fileName);
+  if (file.is_open()) {
     int current_elf_calories = 0;
     std::string line;
-    while (std::getline(file_stream, line)) {
+    while (std::getline(file, line)) {
       if (line.empty()) {
-        if (max_calories < current_elf_calories) { max_calories = current_elf_calories; }
+        elf_calories.emplace_back(current_elf_calories);
         current_elf_calories = 0;
       } else {
         current_elf_calories += std::stoi(line);
       }
     }
-    file_stream.close();
+    if (current_elf_calories != 0) { elf_calories.emplace_back(current_elf_calories); }
+    file.close();
   }
-  return max_calories;
+  std::sort(elf_calories.begin(), elf_calories.end(), std::greater<>());
+  return elf_calories;
+}
+
+std::variant<int, double, std::string> AocDay01::Part1([[maybe_unused]] const std::string &file,
+  [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
+{
+  const auto &calories = DescSortedElfCaloriesFromFile(file);
+  return calories.empty() ? 0 : calories.front(); // Biggest
 }
 
 std::variant<int, double, std::string> AocDay01::Part2([[maybe_unused]] const std::string &file,
   [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
 {
-  std::vector<int> calories;
-  std::ifstream file_stream(file);
-  if (file_stream.is_open()) {
-    int current_elf_calories = 0;
-    std::string line;
-    while (std::getline(file_stream, line)) {
-      if (line.empty()) {
-        calories.emplace_back(current_elf_calories);
-        current_elf_calories = 0;
-      } else {
-        current_elf_calories += std::stoi(line);
-      }
-    }
-    if (current_elf_calories != 0) { calories.emplace_back(current_elf_calories); }
-    file_stream.close();
-  }
-  std::sort(calories.begin(), calories.end(), std::greater<>());
-  return calories.empty() ? 0 : std::accumulate(calories.begin(), std::next(calories.begin(), 3), 0);
+  const auto &calories = DescSortedElfCaloriesFromFile(file);
+  return calories.empty()
+           ? 0
+           : std::accumulate(calories.begin(), std::next(calories.begin(), 3), 0); // Sum of the three biggest
 }
