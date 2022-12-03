f87f51170c17ebe07756c79b6c6200f3d846754a
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-12-03 13:21:54 +0000

    Day 3 part 2

--- src/day_solutions/day_03/aoc_day_03.cpp
+++ src/day_solutions/day_03/aoc_day_03.cpp
@@ -55,5 +55,56 @@ std::variant<int, double, std::string> AocDay03::Part1([[maybe_unused]] const st
 std::variant<int, double, std::string> AocDay03::Part2([[maybe_unused]] const std::string &file,
   [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
 {
-  return 0;
+  constexpr int kStartUpperCase = 27;
+  constexpr int kStartLowerCase = 1;
+  constexpr int kAsciiUpperA = static_cast<int>('A');
+  constexpr int kAsciiLowerA = static_cast<int>('a');
+
+  int items_sum = 0;
+  std::ifstream file_stream(file);
+  if (file_stream.is_open()) {
+    std::vector<std::vector<char>> lines_of_chars;
+    std::string line;
+    while (std::getline(file_stream, line)) {
+      lines_of_chars.emplace_back(line.begin(), line.end());
+
+      if (lines_of_chars.size() == 3) {
+        std::vector<char> intersection_between_first_two;
+        // Sort is needed for set_intersection
+        std::sort(lines_of_chars.at(0).begin(), lines_of_chars.at(0).end());
+        std::sort(lines_of_chars.at(1).begin(), lines_of_chars.at(1).end());
+        std::set_intersection(lines_of_chars.at(0).begin(),
+          lines_of_chars.at(0).end(),
+          lines_of_chars.at(1).begin(),
+          lines_of_chars.at(1).end(),
+          std::back_inserter(intersection_between_first_two));
+        // Remove duplicates from intersection vector
+        intersection_between_first_two.erase(
+          unique(intersection_between_first_two.begin(), intersection_between_first_two.end()),
+          intersection_between_first_two.end());
+
+        std::vector<char> intersection_between_first_two_and_three;
+        // Sort is needed for set_intersection
+        std::sort(intersection_between_first_two.begin(), intersection_between_first_two.end());
+        std::sort(lines_of_chars.at(2).begin(), lines_of_chars.at(2).end());
+        std::set_intersection(intersection_between_first_two.begin(),
+          intersection_between_first_two.end(),
+          lines_of_chars.at(2).begin(),
+          lines_of_chars.at(2).end(),
+          std::back_inserter(intersection_between_first_two_and_three));
+        // Remove duplicates from intersection vector
+        intersection_between_first_two_and_three.erase(
+          unique(intersection_between_first_two_and_three.begin(), intersection_between_first_two_and_three.end()),
+          intersection_between_first_two_and_three.end());
+
+        for (const auto &item : intersection_between_first_two_and_three) {
+          items_sum += (static_cast<bool>(std::isupper(item)))
+                         ? static_cast<int>(item) - kAsciiUpperA + kStartUpperCase
+                         : static_cast<int>(item) - kAsciiLowerA + kStartLowerCase;
+        }
+        lines_of_chars.clear();
+      }
+    }
+  }
+  return items_sum;
 }
--- test/day_solutions/day_03/aoc_day_03_tests.cpp
+++ test/day_solutions/day_03/aoc_day_03_tests.cpp
@@ -50,7 +50,7 @@ TEST_CASE("Solve simple puzzle of the 03 day - part 2", "[day03]")
 {
   AocDays days;
   REQUIRE(
-    0
+    70
     == std::get<int>(days.GetDay(3)->SolvePart(2, "/workspaces/AdventOfCode2022/puzzle_inputs/day_03/simple.txt", {})));
 }
 
@@ -58,6 +58,6 @@ TEST_CASE("Solve actual puzzle of the 03 day - part 2", "[day03]")
 {
   AocDays days;
   REQUIRE(
-    0
+    2780
     == std::get<int>(days.GetDay(3)->SolvePart(2, "/workspaces/AdventOfCode2022/puzzle_inputs/day_03/puzzle.txt", {})));
 }
