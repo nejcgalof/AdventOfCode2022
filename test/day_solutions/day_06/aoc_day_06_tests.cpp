540e94b243c310d766bb638468aeb92021d112f2
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-12-06 11:01:21 +0000

    Add puzzles for day6

--- test/day_solutions/day_06/aoc_day_06_tests.cpp
+++ test/day_solutions/day_06/aoc_day_06_tests.cpp
@@ -34,8 +34,40 @@ TEST_CASE("Solve simple puzzle of the 06 day - part 1", "[day06]")
 {
   AocDays days;
   REQUIRE(
-    0
+    7
     == std::get<int>(days.GetDay(6)->SolvePart(1, "/workspaces/AdventOfCode2022/puzzle_inputs/day_06/simple.txt", {})));
+}
+
+TEST_CASE("Solve simple2 puzzle of the 06 day - part 1", "[day06]")
+{
+  AocDays days;
+  REQUIRE(5
+          == std::get<int>(
+            days.GetDay(6)->SolvePart(1, "/workspaces/AdventOfCode2022/puzzle_inputs/day_06/simple2.txt", {})));
+}
+
+TEST_CASE("Solve simple3 puzzle of the 06 day - part 1", "[day06]")
+{
+  AocDays days;
+  REQUIRE(6
+          == std::get<int>(
+            days.GetDay(6)->SolvePart(1, "/workspaces/AdventOfCode2022/puzzle_inputs/day_06/simple3.txt", {})));
+}
+
+TEST_CASE("Solve simple4 puzzle of the 06 day - part 1", "[day06]")
+{
+  AocDays days;
+  REQUIRE(10
+          == std::get<int>(
+            days.GetDay(6)->SolvePart(1, "/workspaces/AdventOfCode2022/puzzle_inputs/day_06/simple4.txt", {})));
+}
+
+TEST_CASE("Solve simple5 puzzle of the 06 day - part 1", "[day06]")
+{
+  AocDays days;
+  REQUIRE(11
+          == std::get<int>(
+            days.GetDay(6)->SolvePart(1, "/workspaces/AdventOfCode2022/puzzle_inputs/day_06/simple5.txt", {})));
 }
 
 TEST_CASE("Solve actual puzzle of the 06 day - part 1", "[day06]")
