3cdb307989e8981b5971c45cd95f160ba3d41a30
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-12-08 18:03:06 +0000

    Day08 optimize

--- include/day_solutions/day_08/aoc_day_08.hpp
+++ include/day_solutions/day_08/aoc_day_08.hpp
@@ -16,11 +16,14 @@ private:
   std::variant<int, double, std::string> Part2([[maybe_unused]] const std::string &file,
     [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs) override;
 
+  std::tuple<bool, int> CheckTop(const size_t row, const size_t col);
+  std::tuple<bool, int> CheckBottom(const size_t row, const size_t col);
+  std::tuple<bool, int> CheckLeft(const size_t row, const size_t col);
+  std::tuple<bool, int> CheckRight(const size_t row, const size_t col);
+
+  void GenerateTreesFromFile(const std::string &file);
+
   std::vector<std::vector<int>> trees;
-  std::tuple<bool, int> CheckTop(size_t row, size_t col);
-  std::tuple<bool, int> CheckBottom(size_t row, size_t col);
-  std::tuple<bool, int> CheckLeft(size_t row, size_t col);
-  std::tuple<bool, int> CheckRight(size_t row, size_t col);
 
 public:
   AocDay08();
--- src/day_solutions/day_08/aoc_day_08.cpp
+++ src/day_solutions/day_08/aoc_day_08.cpp
@@ -13,7 +13,7 @@ AocDay08::AocDay08() : AocDay(8) {}
 AocDay08::~AocDay08() = default;
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-std::tuple<bool, int> AocDay08::CheckTop(size_t row, size_t col)
+std::tuple<bool, int> AocDay08::CheckTop(const size_t row, const size_t col)
 {
   for (size_t curr_row = row; curr_row-- > 0;) {
     if (trees.at(curr_row).at(col) >= trees.at(row).at(col)) { return std::tuple<bool, int>{ false, row - curr_row }; }
@@ -22,7 +22,7 @@ std::tuple<bool, int> AocDay08::CheckTop(size_t row, size_t col)
 }
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-std::tuple<bool, int> AocDay08::CheckBottom(size_t row, size_t col)
+std::tuple<bool, int> AocDay08::CheckBottom(const size_t row, const size_t col)
 {
   for (size_t curr_row = row + 1; curr_row < trees.size(); curr_row++) {
     if (trees.at(curr_row).at(col) >= trees.at(row).at(col)) { return std::tuple<bool, int>{ false, curr_row - row }; }
@@ -31,7 +31,7 @@ std::tuple<bool, int> AocDay08::CheckBottom(size_t row, size_t col)
 }
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-std::tuple<bool, int> AocDay08::CheckLeft(size_t row, size_t col)
+std::tuple<bool, int> AocDay08::CheckLeft(const size_t row, const size_t col)
 {
   for (size_t curr_col = col; curr_col-- > 0;) {
     if (trees.at(row).at(curr_col) >= trees.at(row).at(col)) { return std::tuple<bool, int>{ false, col - curr_col }; }
@@ -40,7 +40,7 @@ std::tuple<bool, int> AocDay08::CheckLeft(size_t row, size_t col)
 }
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-std::tuple<bool, int> AocDay08::CheckRight(size_t row, size_t col)
+std::tuple<bool, int> AocDay08::CheckRight(const size_t row, const size_t col)
 {
   for (size_t curr_col = col + 1; curr_col < trees.at(row).size(); curr_col++) {
     if (trees.at(row).at(curr_col) >= trees.at(row).at(col)) { return std::tuple<bool, int>{ false, curr_col - col }; }
@@ -48,10 +48,8 @@ std::tuple<bool, int> AocDay08::CheckRight(size_t row, size_t col)
   return std::tuple<bool, int>{ true, (col == trees.at(row).size() - 1) ? 0 : trees.at(row).size() - 1 - col };
 }
 
-std::variant<int, double, std::string> AocDay08::Part1([[maybe_unused]] const std::string &file,
-  [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
+void AocDay08::GenerateTreesFromFile(const std::string &file)
 {
-  int num_visible_trees = 0;
   std::ifstream file_stream(file);
   if (file_stream.is_open()) {
     std::string line;
@@ -61,15 +59,21 @@ std::variant<int, double, std::string> AocDay08::Part1([[maybe_unused]] const st
         line.begin(), line.end(), std::back_inserter(tree_line), [](const auto &character) { return character - '0'; });
       trees.emplace_back(tree_line);
     }
-
-    for (size_t row = 0; row < trees.size(); row++) {
-      for (size_t col = 0; col < trees.at(row).size(); col++) {
-        num_visible_trees += static_cast<int>(std::get<0>(CheckTop(row, col)) || std::get<0>(CheckBottom(row, col))
-                                              || std::get<0>(CheckLeft(row, col)) || std::get<0>(CheckRight(row, col)));
-      }
-    }
-
     file_stream.close();
+  }
+}
+
+std::variant<int, double, std::string> AocDay08::Part1([[maybe_unused]] const std::string &file,
+  [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
+{
+  GenerateTreesFromFile(file);
+
+  int num_visible_trees = 0;
+  for (size_t row = 0; row < trees.size(); row++) {
+    for (size_t col = 0; col < trees.at(row).size(); col++) {
+      num_visible_trees += static_cast<int>(std::get<0>(CheckTop(row, col)) || std::get<0>(CheckBottom(row, col))
+                                            || std::get<0>(CheckLeft(row, col)) || std::get<0>(CheckRight(row, col)));
+    }
   }
   return num_visible_trees;
 }
@@ -77,26 +81,15 @@ std::variant<int, double, std::string> AocDay08::Part1([[maybe_unused]] const st
 std::variant<int, double, std::string> AocDay08::Part2([[maybe_unused]] const std::string &file,
   [[maybe_unused]] const std::vector<std::variant<int, double, std::string>> &extraArgs)
 {
-  int num_visible_trees = 0;
-  std::ifstream file_stream(file);
-  if (file_stream.is_open()) {
-    std::string line;
-    while (std::getline(file_stream, line)) {
-      std::vector<int> tree_line;
-      std::transform(
-        line.begin(), line.end(), std::back_inserter(tree_line), [](const auto &character) { return character - '0'; });
-      trees.emplace_back(tree_line);
-    }
+  GenerateTreesFromFile(file);
 
-    for (size_t row = 0; row < trees.size(); row++) {
-      for (size_t col = 0; col < trees.at(row).size(); col++) {
-        auto aaa = std::get<1>(CheckTop(row, col)) * std::get<1>(CheckBottom(row, col))
-                   * std::get<1>(CheckLeft(row, col)) * std::get<1>(CheckRight(row, col));
-        if (aaa > num_visible_trees) { num_visible_trees = aaa; }
-      }
+  int highest_scenic_score = 0;
+  for (size_t row = 0; row < trees.size(); row++) {
+    for (size_t col = 0; col < trees.at(row).size(); col++) {
+      const auto scenic_score = std::get<1>(CheckTop(row, col)) * std::get<1>(CheckBottom(row, col))
+                                * std::get<1>(CheckLeft(row, col)) * std::get<1>(CheckRight(row, col));
+      if (scenic_score > highest_scenic_score) { highest_scenic_score = scenic_score; }
     }
-
-    file_stream.close();
   }
-  return num_visible_trees;
+  return highest_scenic_score;
 }
