4332c6458e23d79fca74f5314e5560ee996d8ccb
nejcgalof <galof.nejc@gmail.com/workspaces/AdventOfCode2022/out/build/unixlike-gcc-debug/bin/AdventOfCode2022>
2022-12-17 13:58:54 +0000

    Day 13 - cleanup part1

--- src/day_solutions/day_13/aoc_day_13.cpp
+++ src/day_solutions/day_13/aoc_day_13.cpp
@@ -16,41 +16,30 @@ AocDay13::~AocDay13() = default;
 size_t AocDay13::ParseSequence(List *item, std::string line)
 {
   for (size_t character = 0; character < line.length(); character++) {
-    // std::cout << "Character: " << line.at(character) << std::endl;
     if (line.at(character) == '[') {
-      std::cout << "Start" << std::endl;
       auto children = std::make_unique<List>();
       children->parent = item;
       character += ParseSequence(children.get(), line.substr(character + 1));
-      // std::cout << "Berem: " << children->packetData.size() << std::endl;
       item->packetData.emplace_back(std::move(children));
     } else if (line.at(character) == ']') {
-      std::cout << "End" << std::endl;
       return character + 1;
     } else if (line.at(character) == ',') {
       continue;
     } else {
       auto subs = line.substr(character);
       item->packetData.emplace_back(std::stoi(line.substr(character, subs.find_first_not_of("0123456789"))));
-      std::cout << "Char: " << line.substr(character, subs.find_first_not_of("0123456789")) << std::endl;
       character += subs.find_first_not_of("0123456789") - 1;
     }
   }
-  // std::cout << "Poglejmo: " << item->packetData.size() << std::endl;
   return line.length();
 }
 
 int AocDay13::CheckEmptiness(List *firstPacket, List *secondPacket)
 {
-  if (firstPacket->packetData.empty() && secondPacket->packetData.empty()) {
-    // std::cout << "both packets are empty" << std::endl;
-    return -1;
-  }
+  if (firstPacket->packetData.empty() && secondPacket->packetData.empty()) { return -1; }
   if (firstPacket->packetData.empty()) {
-    // std::cout << "first packet are empty" << std::endl;
     return 1;
   } else if (secondPacket->packetData.empty()) {
-    // std::cout << "second packet are empty" << std::endl;
     return 0;
   }
   return -2;
@@ -67,16 +56,13 @@ int AocDay13::CheckNullptr(List *firstPacket, List *secondPacket)
 
 int AocDay13::ComparePackets(List *firstPacket, List *secondPacket)
 {
-  int nullness = CheckNullptr(firstPacket, secondPacket);
+  const int nullness = CheckNullptr(firstPacket, secondPacket);
   if (nullness >= 0) { return nullness; }
-  // std::cout << "ComparePackets" << std::endl;
   const int check_emptiness = CheckEmptiness(firstPacket, secondPacket);
   if (check_emptiness == -1) {
-    // std::cout << "Move out" << std::endl;
-    // std::cout << "Size first parent: " << firstPacket->parent->packetData.size() << std::endl;
     auto *first_parent = firstPacket->parent;
     auto *second_parent = secondPacket->parent;
-    int parent_nullness = CheckNullptr(first_parent, second_parent);
+    const int parent_nullness = CheckNullptr(first_parent, second_parent);
     if (parent_nullness >= 0) { return parent_nullness; }
     if (!first_parent->packetData.empty()) { first_parent->packetData.erase(first_parent->packetData.begin()); }
     if (!second_parent->packetData.empty()) { second_parent->packetData.erase(second_parent->packetData.begin()); }
@@ -87,15 +73,12 @@ int AocDay13::ComparePackets(List *firstPacket, List *secondPacket)
 
   if (std::holds_alternative<std::unique_ptr<List>>(firstPacket->packetData.at(0))
       && std::holds_alternative<std::unique_ptr<List>>(secondPacket->packetData.at(0))) {
-    // std::cout << "both values are lists" << std::endl;
     return ComparePackets(std::get<std::unique_ptr<List>>(firstPacket->packetData.at(0)).get(),
       std::get<std::unique_ptr<List>>(secondPacket->packetData.at(0)).get());
   } else if (std::holds_alternative<int>(firstPacket->packetData.at(0))
              && std::holds_alternative<int>(secondPacket->packetData.at(0))) {
-    // std::cout << "both values are integers" << std::endl;
     auto first_number = std::get<int>(firstPacket->packetData.at(0));
     auto second_number = std::get<int>(secondPacket->packetData.at(0));
-    std::cout << "compare: " << first_number << " " << second_number << std::endl;
     if (first_number < second_number) {
       return 1;
     } else if (first_number > second_number) {
@@ -106,20 +89,16 @@ int AocDay13::ComparePackets(List *firstPacket, List *secondPacket)
       return ComparePackets(firstPacket, secondPacket);
     }
   } else {
-    // std::cout << "Mixed type" << std::endl;
     if (std::holds_alternative<int>(firstPacket->packetData.at(0))) {
-      // std::cout << "First is int" << std::endl;
       auto new_one = std::make_unique<List>();
       new_one->packetData.emplace_back(std::move(firstPacket->packetData.at(0)));
       firstPacket->packetData.at(0) = std::move(new_one);
     } else if (std::holds_alternative<int>(secondPacket->packetData.at(0))) {
-      // std::cout << "Second is int" << std::endl;
       auto new_one = std::make_unique<List>();
       new_one->packetData.emplace_back(std::move(secondPacket->packetData.at(0)));
       secondPacket->packetData.at(0) = std::move(new_one);
     }
     return ComparePackets(firstPacket, secondPacket);
-    // topPriorityRules.erase(topPriorityRules.begin());
   }
   return 0;
 }
@@ -141,13 +120,12 @@ std::variant<int, double, std::string> AocDay13::Part1([[maybe_unused]] const st
     file_stream.close();
 
     for (size_t pairs = 0; pairs < packets.size() / 2; pairs++) {
-      size_t move = pairs * 2;
-      int res = ComparePackets(packets.at(move).get(), packets.at(move + 1).get());
-      std::cout << "Result: " << res << std::endl;
-      if (res == 1) { result += static_cast<int>(pairs + 1); }
+      const size_t move = pairs * 2;
+      if (ComparePackets(packets.at(move).get(), packets.at(move + 1).get()) == 1) {
+        result += static_cast<int>(pairs + 1);
+      }
     }
   }
-
   return result;
 }
 
